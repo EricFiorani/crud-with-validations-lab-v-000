3e198eac158253de7e6a680dbe2267d879c8bba5
Eric Fiorani <efiorani305@gmail.com>
2018-11-14 15:31:20 +0000

    automatically backed up by learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -5,5 +5,8 @@ class Song < ActiveRecord::Base
     message: "cannot be repeated by the same artist in the same year"
   }
   validates :released, inclusion: { in: [true, false]}
-  
+  validates :release_year, presence: true, if: :released?
+  validates :release_year, numericality: { less_then: Date.today.year }, if: :released?
+  valdates :artist_name, presence: true
+  validates :genre, presence: true
 end
