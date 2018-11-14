292eed6f300d08b5688d0de12020d06315e1f240
Eric Fiorani <efiorani305@gmail.com>
2018-11-14 15:31:55 +0000

    automatically backed up by learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -7,7 +7,7 @@ class Song < ActiveRecord::Base
   validates :released, inclusion: { in: [true, false]}
   validates :release_year, presence: true, if: :released?
   validates :release_year, numericality: { less_then: Date.today.year }, if: :released?
-  valdates :artist_name, presence: true
+  validates :artist_name, presence: true
   validates :genre, presence: true
 
   def released?
